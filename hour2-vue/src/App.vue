870341d453ea61c59e914eb35f391e128d735f70
j0hnxie <johnjxie@gmail.com>
2026-02-05 18:43:01 -0500

    Initial commit

---
+++ hour2-vue/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+function reset() {
+  count.value = 0;
+}
+</script>
+
+<template>
+  <div class="card">
+    <h1>Vue Counter</h1>
+
+    <!-- EXERCISE 7: display count -->
+    <p>Count: {{ count }}</p>
+
+    <!-- EXERCISE 7: increment button -->
+    <button @click="increment">Increment</button>
+
+    <!-- EXERCISE 8: conditional reset -->
+    <button v-if="count > 0" @click="reset">Reset</button>
+
+    <!-- EXERCISE 8: threshold message -->
+    <p v-if="count >= 10">🎉 You reached 10!</p>
+  </div>
+</template>
